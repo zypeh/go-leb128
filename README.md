8ca2c666434448355657e6fd3b1575e33fe9cc4c
zypeh <zypeh.geek@gmail.com>
2020-05-01 16:21:36 +0800

    decode and append done; README updated

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # go-leb128
-Little Endian Base 128, a variable-length code compression for arbitrarily large integer in a small number of bytes.
+> Little Endian Base 128, a variable-length code compression for arbitrarily large integer in a small number of bytes.
+
+# Usage 
+The bytes payload is typed in this library. So instead of using `[]byte` we are using
+
+* `SLeb128` for Signed LEB128 encoding
+* `ULeb128` for Unsigned LEB128 encoding
+
+So for unsigned LEB128 encoding it should be written in
+
+```go
+sleb624485 := ULeb128{[]byte{0xE5, 0x8E, 0x26}}
+```
+
+And it cannot be consumed by signed LEB128 decoder (typed)
+
+```go
+DecodeSLeb128(sleb624485) // this will not compile
+DecodeULeb128(sleb624485) // compiles !
+```
+
+### Encoding
+
+**EncodeFromUint64(uint64) ULeb128**
+Encode `uint64` typed integer into `ULeb128`
+
+**EncodeFromInt64(int64) SLeb128**
+Encode `int64` typed integer into `SLeb128`
+
+
+### Decoding
+
+**DecodeULeb128(ULeb128) (uint64, error)**
+Decode `ULeb128` to uint64 with error flag
+
+**DecodeSLeb128(SLeb128) (int64, error)**
+Decode `SLeb128` to int64 with error flag
+
+> If the error is not equal to nil (means error occurred). The result is always 0
+
+### Utilities
+
+**AppendSLeb128(x, y SLeb128) SLeb128**
+Append two `Sleb128` into one `SLeb128`
+
+**AppendULeb128(x, y ULeb128) ULeb128**
+Append two `Uleb128` into one `ULeb128`
--- leb128.go
+++ leb128.go
@@ -1,5 +1,9 @@
 package leb128
 
+import (
+	"errors"
+)
+
 var sevenbits = [...]byte{
 	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
 	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
@@ -11,27 +15,43 @@ var sevenbits = [...]byte{
 	0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f,
 }
 
+type bs []byte
+
 // SLeb128 : There are 2 versions of LEB128: unsigned LEB128 and signed128.
 // The decoder must know whether the encoded value is unsigned or signed.
-type SLeb128 []byte
+type SLeb128 struct {
+	bs
+}
 
 // ULeb128 : Unsigned LEB128
-type ULeb128 []byte
+type ULeb128 struct {
+	bs
+}
 
-// EncodeFromUint64 :
+// AppendSLeb128 : Append two `Sleb128` into one `SLeb128`
+func AppendSLeb128(x, y SLeb128) SLeb128 {
+	return SLeb128{append(x.bs, y.bs...)}
+}
+
+// AppendULeb128 : Append two `Uleb128` into one `ULeb128`
+func AppendULeb128(x, y ULeb128) ULeb128 {
+	return ULeb128{append(x.bs, y.bs...)}
+}
+
+// EncodeFromUint64 : Encode `uint64` typed integer into `ULeb128`
 func EncodeFromUint64(input uint64) ULeb128 {
 	// https://golang.org/src/cmd/internal/dwarf/dwarf.go
 	if input < 0x80 {
-		return []byte{sevenbits[input]}
+		return ULeb128{[]byte{sevenbits[input]}}
 	}
 
 	// https://en.wikipedia.org/wiki/LEB128
 	var bytes []byte
 	for {
 		b := uint8(input & 0x7f)
-		v := input >> 7
+		input = input >> 7
 
-		if v != 0 {
+		if input != 0 {
 			b |= 0x80
 		}
 
@@ -40,24 +60,24 @@ func EncodeFromUint64(input uint64) ULeb128 {
 			break
 		}
 	}
-	return bytes
+	return ULeb128{bytes}
 }
 
-// EncodeFromInt64 :
+// EncodeFromInt64 : Encode `int64` typed integer into `SLeb128`
 func EncodeFromInt64(input int64) SLeb128 {
 	if input >= 0 && input <= 0x3f {
-		return []byte{sevenbits[input]}
+		return SLeb128{[]byte{sevenbits[input]}}
 	} else if input < 0 && input >= ^0x3f {
-		return []byte{sevenbits[0x80+input]}
+		return SLeb128{[]byte{sevenbits[0x80+input]}}
 	}
 
 	var bytes []byte
 	for {
 		b := uint8(input & 0x7f)
 		signBit := uint8(input & 0x40) // sign bit is second high order bit
-		v := input >> 7
+		input = input >> 7
 
-		if (v != 0 || signBit == 0) && (v != -1 || signBit == 0) {
+		if (input != 0 || signBit != 0) && (input != -1 || signBit == 0) {
 			b |= 0x80 // set high order bit of byte
 		}
 
@@ -66,15 +86,51 @@ func EncodeFromInt64(input int64) SLeb128 {
 			break
 		}
 	}
-	return bytes
+	return SLeb128{bytes}
 }
 
-// DecodeFromULeb128 :
-func DecodeFromULeb128(src ULeb128) uint64 {
-	return 0
+// DecodeULeb128 :
+func DecodeULeb128(src ULeb128) (result uint64, err error) {
+	length := uint8(len(src.bs) & 0xff)
+	if length > 10 {
+		length = 10
+	}
+
+	var i uint8
+	for i = 0; i < length; i++ {
+		result |= uint64(src.bs[i]&0x7f) << (7 * i) // shift
+		if src.bs[i]&0x80 == 0 {
+			if uint8(i+1) == 0 {
+				result = 0
+				err = errors.New("bad encoding")
+				return
+			}
+		}
+
+	}
+	return
 }
 
-// DecodeFromSLeb128 :
-func DecodeFromSLeb128(src SLeb128) int64 {
-	return 0
+// DecodeSLeb128 :
+func DecodeSLeb128(src SLeb128) (result int64, err error) {
+	length := uint8(len(src.bs) & 0xff)
+	if length > 10 {
+		length = 10
+	}
+
+	var i uint8
+	for i = 0; i < length; i++ {
+		result |= int64(src.bs[i]&0x7f) << (7 * i)
+		if src.bs[i]&0x80 == 0 {
+			if src.bs[i]&0x40 != 0 { // bitflag
+				result |= ^0 << (7 * (i + 1))
+			}
+			if uint8(i+1) == 0 {
+				result = 0
+				err = errors.New("bad encoding")
+				return
+			}
+		}
+	}
+	return
 }
